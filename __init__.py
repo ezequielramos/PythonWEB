08f3ef0139433d4a0a4b7e9c9fac47d10c405c28
Ezequiel Ramos <ezequielmr94@gmail.com>
2017-09-23 19:41:00 -0300

    bancodedados

--- __init__.py
+++ __init__.py
@@ -1,30 +1,50 @@
+import sqlite3
 from flask import Flask, request, render_template
+
+#conn
+
+conn = sqlite3.connect('test.db')
+c = conn.cursor()
+
+c.execute("SELECT name FROM sqlite_master WHERE type='table' AND NAME='users' ")
+
+if not c.fetchone():
+    c.execute("CREATE TABLE users(user text, password text)")
+
+conn.close()
+
+#form
+
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
 def index():
-	return "Hello World!"
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+    user = request.args.get('user')
+    password = request.args.get('pass')
 
-@app.route('/user/<username>')
-def show_user_profile(username):
-    return 'User ' + username
+    conn = sqlite3.connect('test.db')
+    
+    if user and password:
+        c = conn.cursor()
+        c.execute("INSERT INTO users (user, password) VALUES(?,?)",(user,password))
+        conn.commit()
 
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    return 'Post ' + str(post_id)
+    conn.close()
+        
+    return render_template('index.html',user=user,password=password)
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        #do_the_login()
-        pass
-    else:
-        #show_the_login_form()
-        return "login_form"
+#list
+@app.route('/list')
+def list():
+
+    conn = sqlite3.connect('test.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM users")
+    users = c.fetchall()
+    conn.close()
+
+    return render_template('list.html',users=users)
 
 app.run(debug=True)
---
+++ steps/Step6/Step6.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, render_template
+app = Flask(__name__)
+
+#form
+
+@app.route('/')
+@app.route('/index')
+def index():
+
+	user = request.args.get('user')
+	password = request.args.get('pass')
+
+	if user and password:
+		users.append({
+			"user":user,
+			"password":password
+		})
+
+	return render_template('index.html',user=user,password=password)
+
+#list
+@app.route('/list')
+def list():
+	return render_template('list.html',users=users)
+
+users = []
+
+app.run(debug=True)
