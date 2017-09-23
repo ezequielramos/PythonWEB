17011e90b8607e77e435738f6bcc201251886dda
Ezequiel Ramos <ezequielmr94@gmail.com>
2017-09-23 17:30:43 -0300

    step4

---
+++ steps/Step4.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__)
+
+#Path Params
+
+@app.route('/sayYourName/<name>')
+def sayYourName(name):
+	return '''<html>
+<head>
+	<title>''' + name + '''</title>
+</head>
+<body>
+	<h1>Hello, ''' + name + '''</h1>
+</body>
+</html>'''
+
+app.run(debug=True)
